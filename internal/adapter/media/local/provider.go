10d162ca6db4e434d7f7fa6fc15ecc8b80d67a80
Maxime Guidon <guidonm@gmail.com>
2026-03-12 08:04:33 +0000

    Revert "83: add compile-time interface assertion for ports.MediaProvider"
    This reverts commit 03b18e9beb38bb1ef94c37b6ce828ce5e541667b.

--- internal/adapter/media/local/provider.go
+++ internal/adapter/media/local/provider.go
@@ -4,11 +4,7 @@ import (
 	"context"
 	"errors"
 	"io"
-
-	"github.com/outfitte/outfitte/internal/ports"
 )
-
-var _ ports.MediaProvider = (*Provider)(nil)
 
 // Provider is a local filesystem-backed implementation of ports.MediaProvider.
 // Media files are stored under root, keyed by their path segment.
